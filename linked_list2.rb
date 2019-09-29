5c9d5509ef0b87349d3b04b055e927a96846abea
Sean O'Neil <sean.oneil424@gmail.comgit config --global user.name Sean>
2019-09-29 21:14:58 +0000

    linked_list2

---
+++ linked_list2.rb
@@ -0,0 +1,47 @@
+class LinkedListNode
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node=nil)
+    @value = value
+    @next_node = next_node
+  end
+end
+
+
+def print_values(list_node)
+  if list_node
+    print "#{list_node.value} --> "
+    print_values(list_node.next_node)
+  else
+    print "nil\n"
+    return
+  end
+end
+
+def reverse_list(current_node, previous=nil)
+  while current_node != nil do
+    next_node = current_node.next_node 
+    current_node.next_node = previous 
+    previous = current_node
+    current_node = next_node
+  end
+  return previous
+end
+
+node1 = LinkedListNode.new(37)
+node2 = LinkedListNode.new(99, node1)
+node3 = LinkedListNode.new(12, node2)
+
+print_values(node3)
+
+puts "-------"
+
+revlist = reverse_list(node3)
+
+print_values(revlist)
+
+
+
+#last_node
+#current_node
+#next_node
